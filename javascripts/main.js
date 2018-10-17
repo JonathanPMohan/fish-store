bf2fb1424fdbb41cb18146901a4b5fb683036a9b
Jonathan Mohan <JonathanMohan.net@gmail.com>
2018-10-16 19:40:33 -0500

    My Fish Just Died

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,56 +4,62 @@
 const writeFishes = (arrayOfFishes) => {
     let domString = '';
     arrayOfFishes.forEach((fish) => {
-        domString += `
-            <div class="${fish.onSale ? 'on-sale' : '' } fish card col-md-6 col-md-offset-3">
-                <div class="thumbnail">
-                    <img src="${fish.imageSoure}" 
-                        alt="" width="40%">
-                    <div class="caption">
-                        <h3 id="thumbnail-label">${fish.name}</h3>
-                        <p>$
-                            <span class="price">${fish.basePrice}</span>
-                        </p>
-                    </div>
-                    <div class="caption card-footer">
-                        <button class="add btn btn-danger">Do You Like Fishsticks?</button>
-                    </div>
-                </div>
-            </div>
-            `
-
+      domString += `
+      <div class="${fish.onSale ? 'on-sale' : ''} fish card col-md-6 col-md-offset-3">
+      <div class="thumbnail">
+          <img src="${fish.imageSoure}"
+              alt="" width="40%">
+          <div class="caption">
+              <h3 id="thumbnail-label">${fish.name}</h3>
+              <p>$
+                  <span class="price">${fish.basePrice}</span>
+              </p>
+          </div>
+          <div class="caption card-footer">
+              <button class="add btn btn-danger">Do You Like Fishsticks??</button>
+          </div>
+      </div>
+  </div>
+      `
     })
     // Write to the available div
     $("#available").append(domString);
-   bindEvents();
-    }
-
-const bindEvents = () => {
-    $(".add").on('click', (e) => {
-     // what is the div that has the fish
-     const fishToMove = $(e.target).closest('.fish');
-     // move it to the 'snagged' div
-     $("#snagged").append(fishToMove);
-     // button text => Remove from Basket | change class - 'add' + 'remove'
-     $(e.target).text('These Fish Are Dead :(').addClass('remove').removeClass('add');
-    });
-};
-
-// Dynamically listen for events that happen on buttons with a class
-// $('body').on('click', 'button.add', () => {
-
-// })
-
-// Load Fish 
-$.get('../db/fishes.json')
+    // bindEvents();
+  }
+  
+  // const bindEvents = () => {
+  //   $(".add").on('click', (e) => {
+      
+  //   });
+  
+  //   // Remove Fish
+  //   $(".remove").on('click', (e) => {
+      
+  //   })
+  // };
+  
+  // Dynamically listen for events that happen on buttons with a class of add
+  $('body').on('click', 'button.add', (e) => {
+    // what is the div that has the fish
+    const fishToMove = $(e.target).closest('.fish');
+    // move it to the 'snagged' div
+    $("#snagged").append(fishToMove);
+    // button text => Remove from Basket | change class - 'add' + 'remove'
+    $(e.target).text('Sorry, Your Fish Is Dead :(').addClass('remove').removeClass('add');
+  })
+  
+  $('body').on('click', 'button.remove', (e) => {
+    const fishToMove = $(e.target).closest('.fish');
+      $("#available").append(fishToMove);
+      $(e.target).text('Add To Basket').addClass('add').removeClass('remove');
+    })
+  
+  // Load Fish
+  $.get('../db/fishes.json')
     .done((data) => {
-        console.log(data);
-        writeFishes(data.fishes);
+      console.log(data);
+      writeFishes(data.fishes);
     })
     .fail((error) => {
-        console.error(error);
-    });
-
-    $(".add").on('click', (e) => {
-        alert('Do You Like Fish Sticks?')
-    });
+      console.error(error);
+  });
